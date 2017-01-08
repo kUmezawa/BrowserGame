33c0bedb9fff78b3180bf0d4243118f59d1c6fe7
your user name <tsuniki.miniki@gmail.com>
2017-01-08 22:38:41 +0900

    adjuested

--- js/SampleScript.js
+++ js/SampleScript.js
@@ -1,7 +1,12 @@
 /**
  * 
  */
+
+console.log("***** niku *****");
+
 $('#btnTest').bind('click', function(){
 	console.log("console test");
 	alert('jQuery Test');
 });
+
+console.log("***** niku2 *****");

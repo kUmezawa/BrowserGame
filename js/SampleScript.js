a140b0bd830a477e1f96e8806f216e9e69ba3d51
your user name <tsuniki.miniki@gmail.com>
2017-01-08 21:56:39 +0900

    adjested

--- js/SampleScript.js
+++ js/SampleScript.js
@@ -1,21 +1,6 @@
 /**
  * 
  */
-
-//$('#toolContent').show();
-$('#toolTitle').bind('click', function() {
-	//$('#toolContent').show();
-	$('#toolContent').slideDown();
-});
-
-$('#btnClose').bind('click', function(){
-	if(confirm('閉じてよろしいですか?')){
-		window.close();
-	}else{
-		alert('閉じるのを中止しました');
-	}
-});
-
 $('#btnTest').bind('click', function(){
 	console.log("console test");
 	alert('jQuery Test');

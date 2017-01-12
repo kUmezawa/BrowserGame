a8b53c6308f0cdc2092a28c07e678d00f3bbf860
Kazumasa Umezawa <e35372@jp.ibm.com>
2017-01-12 10:54:46 +0900

    ロードされるタイミングで読み込むように修正 bindよりonを使ったほうが良いので修正

--- js/SampleScript.js
+++ js/SampleScript.js
@@ -1,22 +1,34 @@
 /**
  * 
  */
-$('#toolTitle').bind('click', function() {
-	//$('#toolContent').show();
-	$('#toolContent').slideDown();
-});
+ 
+$(function(){
 
-
-$('#btnTest').bind('click', function(){
-	console.log("console test");
-	alert('jQuery Test');
-});
-
-$('#btnClose').bind('click', function(){
+	$('#toolTitle').on('click',function() {
+		//$('#toolContent').show();
+		console.log("hoge");
+		$('#toolContent').slideDown();
+	});
+	
+	$('#btnTest').on('click',function(){
+		console.log("console test");
+		alert('jQuery Test');
+	});
+	
+	$('#btnClose').on('click', function(){
 	if(confirm('閉じてよろしいですか?')){
 		window.close();
 	}else{
 		alert('閉じるのを中止しました');
 	}
+    });
+
 });
+
+
+
+
+
+
+
 
